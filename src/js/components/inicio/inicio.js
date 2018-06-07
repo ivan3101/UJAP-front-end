6b9e9af86e4af1ca41278681710cb8f0ec59cf56
Alirio A. Angel <alirioangel.1993@gmail.com>
2018-06-06 21:54:11 -0400

    Signed-off-by: Alirio A. Angel

--- src/js/components/inicio/inicio.js
+++ src/js/components/inicio/inicio.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import {Container,Row,Col } from 'reactstrap';
 
 class Inicio extends Component {
     render = () => (
-        <div>
-            <h1>Bienvenido a UJAP en lina</h1>
+        <div className="inicio">
+            <Container fluid>
+            <Row>
+                <Col md="8"> 
+                    <h1>Noticias</h1>
+                
+                </Col>
+                <Col md="4">
+                    <h1>Segunda parte</h1>
+                </Col>
+            </Row>
+            </Container>
         </div>
     )
 }
